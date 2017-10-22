c20fca2fa4fba5a8b900729c0dae5cb658cf0225
joycetruong <joyce.truong591@gmail.com>
2017-10-22 02:03:48 -0400

    function connection update

--- js/userSDR.js
+++ js/userSDR.js
@@ -15,4 +15,33 @@ function userSDR () {
       c.end ();
     });
   }
+
+  this.delete = function (user, callback) {
+
+    var c = connection.connected () ;
+
+    c.query ("INSERT INTO user SET?"), [user], function (err, result) {
+      if (err) {
+        console.log ("[ERROR] " + err.message);
+        callback (false);
+      } else
+        callback (true);
+      c.end ();
+    });
+  }
+
+  this.refresh = function (user, callback) {
+
+    var c = connection.connected () ;
+
+    c.query ("INSERT INTO user SET?"), [user], function (err, result) {
+      if (err) {
+        console.log ("[ERROR] " + err.message);
+        callback (false);
+      } else
+        callback (true);
+      c.end ();
+    });
+  }
+
 }
